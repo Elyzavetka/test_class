52414237eb6245ba0980b42b498d55be268e4863
Liza Tarasova <tarasova.liza.vic@gmail.com>
2025-06-12 16:28:38 +0100

    Add tests and Music class

---
+++ test/test_music_class.py
@@ -0,0 +1,28 @@
+from lib.music_class import *
+
+def test_music_class_exist():
+    result = Music()
+    assert isinstance(result, Music) == True
+
+def test_return_empty_song_list():
+    result = Music()
+    result.show_songs()
+    assert result.list_track == []
+
+def test_add_song():
+    result = Music()
+    result.add("Desert rose")
+    assert result.list_track == ["Desert rose"]
+
+def test_return_list_of_one_song():
+    result = Music()
+    result.add("Desert rose")
+    result.show_songs()
+    assert result.list_track == ["Desert rose"]
+
+def test_return_list_of_two_songs():
+    result = Music()
+    result.add("Desert rose")
+    result.add("On the floor")
+    result.show_songs()
+    assert result.list_track == ["Desert rose", "On the floor"]
